a8a39668568ffc5d61bd9ecfa6ba9aee84b86db8
hamzaplojovic <hamzaplojovic9@gmail.com>
2022-10-01 14:18:22 +0200

    approve reject

--- repo/admin.py
+++ repo/admin.py
@@ -6,14 +6,12 @@ db = deta.Base("users")
 
 
 def user_action(username, value):
-    user = db.fetch({"username": username}).items
-    if user[0]:
-        user[0]["is_active"] = value
-        return user
+    user = db.fetch({"username": username}).items[0]
+    user["is_active"] = value
+    return db.put(user, key=user["username"])
 
 
 def login(username, password):
-
     user = db.fetch({"username": username, "password": sha256(
         password.encode("utf-8")).hexdigest()}).items
     return 200 if user[0]["role"] == "admin" else 404
