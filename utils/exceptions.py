85509e9c1e6f4047dc52f119dcae5a96f83d7642
hamzaplojovic <hamzaplojovic9@gmail.com>
2022-10-30 12:10:52 +0100

    user exceptions

---
+++ utils/exceptions.py
@@ -0,0 +1,13 @@
+from fastapi import HTTPException, status
+
+class UserExceptions:
+    def __init__(self):
+        self.name = "user exceptions"
+
+    def raise_not_found(message):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=message)
+    
+    def raise_conflict(message):
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                            detail=message)
